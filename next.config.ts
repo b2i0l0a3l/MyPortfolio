5bd4b60cbd31a60352e322f5b11392102dd1c4ec
Bilal <animeotaku304@gmail.com>
2026-06-08 13:56:29 +0100

    feat: add @prisma/client to serverExternalPackages in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["react-icons", "lucide-react"],
   },
+  serverExternalPackages: ["@prisma/client"],
   images: {
     remotePatterns: [
       {
